de9f436e13a0e49edba73610582ad3ab1ce97b9a
Nikolay Kukhta <nik.nosferatu@gmail.com>
2014-05-10 00:41:01 +0300

    Fixed chash when pilots count less than requested top

--- RC_Drift_Judgment_backend/views.py
+++ RC_Drift_Judgment_backend/views.py
@@ -29,45 +29,44 @@ def results(request):
 def topResults(request,topNum):
 
     drivers = getQualifyResults()
-
     topNum = int(topNum)
-    N = topNum
-    top = {}
-
-    pilotsCounter = 0
-    shift = -2
-
-    for i in xrange(0, N / 8):
-        position = i * 2
-
-        top[position] = drivers[pilotsCounter]
-        pilotsCounter += 1
-        position += N / 2
-
-        top[position] = drivers[pilotsCounter]
-        pilotsCounter += 1
-        position += shift
-
-        top[position] = drivers[pilotsCounter]
-        pilotsCounter += 1
-        position += N / 2
-
-        top[position] = drivers[pilotsCounter]
-        pilotsCounter += 1
-
-        shift -= 4
-
-    for i in xrange(0, N, 2):
-        pilotPosition = drivers.index(top[i]) + 1
-        pairPosition = N - pilotPosition
-
-        top[i + 1] = drivers[pairPosition]
 
     pairs = []
-    for i in xrange(0,len(top.keys()),2):
-        pairs.append( (top[i],top[i+1] ))
+    if topNum <= len(drivers):
+        N = topNum
+        top = {}
 
-    print pairs
+        pilotsCounter = 0
+        shift = -2
+
+        for i in xrange(0, N / 8):
+            position = i * 2
+
+            top[position] = drivers[pilotsCounter]
+            pilotsCounter += 1
+            position += N / 2
+
+            top[position] = drivers[pilotsCounter]
+            pilotsCounter += 1
+            position += shift
+
+            top[position] = drivers[pilotsCounter]
+            pilotsCounter += 1
+            position += N / 2
+
+            top[position] = drivers[pilotsCounter]
+            pilotsCounter += 1
+
+            shift -= 4
+
+        for i in xrange(0, N, 2):
+            pilotPosition = drivers.index(top[i]) + 1
+            pairPosition = N - pilotPosition
+
+            top[i + 1] = drivers[pairPosition]
+
+        for i in xrange(0,len(top.keys()),2):
+            pairs.append( (top[i],top[i+1] ))
 
     return render_to_response(
         "top.html",
